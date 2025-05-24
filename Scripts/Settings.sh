32771cc602896a21a73871005aea6c16dbe4192d
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2025-05-24 19:11:12 +0800

    Update Settings.sh
    #编译器优化

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -65,7 +65,8 @@ fi
 if [[ $WRT_TARGET != *"X86"* ]]; then
 	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
  	#------------2025.05.24---------------￥
-	#echo "CONFIG_TARGET_OPTIMIZATION=\"-O3 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53 -mtune=cortex-a53\"" >> ./.config
+  	#（初始-正常）echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+	#（尝试-失败）echo "CONFIG_TARGET_OPTIMIZATION=\"-O3 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53 -mtune=cortex-a53\"" >> ./.config
  	echo 'CONFIG_TARGET_OPTIMIZATION="-O3 -pipe -mcpu=cortex-a53 -mtune=cortex-a53"' >> ./.config
   	#------------2025.05.24---------------￥
 fi
