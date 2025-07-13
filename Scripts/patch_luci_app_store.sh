5727ec5ab5d76d98b5e4f05ff4182ba18a6ecabd
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2025-07-13 23:40:16 +0800

    Update patch_luci_app_store.sh

--- Scripts/patch_luci_app_store.sh
+++ Scripts/patch_luci_app_store.sh
@@ -15,12 +15,10 @@
   ##exit 1
 ##fi
 
-# 遍历 package 和 feeds 目录下所有 Makefile ---- 针对所有
+# 遍历 package 和 feeds 目录下所有 Makefile，修正 PKG_VERSION 和 PKG_RELEASE 格式
 find ./ -type f -name Makefile | while read MKFILE; do
-    # 检查是否包含不规范的 PKG_VERSION (比如有"-"号)
     if grep -qE '^PKG_VERSION:=[0-9]+\.[0-9]+\.[0-9]+-[0-9]+' "$MKFILE"; then
         echo "Patching version in $MKFILE"
-        # 提取主版本和release号
         sed -i -E 's/^(PKG_VERSION:=)([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+)/\1\2\nPKG_RELEASE:=\3/' "$MKFILE"
     fi
 done
