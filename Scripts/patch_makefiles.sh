acc8137957daa50851aa74fbfee767c86de90130
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2025-07-13 19:02:26 +0800

    Update patch_makefiles.sh

--- Scripts/patch_makefiles.sh
+++ Scripts/patch_makefiles.sh
@@ -2,14 +2,14 @@
 
 # Patch luci-app-openvpn-server & luci-app-openvpn: 禁止重复安装 /etc/config/openvpn
 for PKGNAME in luci-app-openvpn-server luci-app-openvpn; do
-  find ./ -type f -name Makefile -path "*/$PKGNAME/Makefile" | while read MK; do
+  find . -type f -path "*/$PKGNAME/Makefile" | while read MK; do
     echo "Patching $MK"
     sed -i '/INSTALL_CONF.*openvpn.config.*etc\/config\/openvpn/s/^/#/' "$MK"
   done
 done
 
 # Patch luci-app-socat: 禁止重复安装 /etc/config/socat
-find ./ -type f -name Makefile -path "*/luci-app-socat/Makefile" | while read MK; do
+find . -type f -path "*/luci-app-socat/Makefile" | while read MK; do
   echo "Patching $MK"
   sed -i '/INSTALL_CONF.*socat.config.*etc\/config\/socat/s/^/#/' "$MK"
 done
