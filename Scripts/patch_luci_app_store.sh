424a8cd8845a5ce515bb3b6f18c3b6b82be8dce7
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2025-07-14 00:13:54 +0800

    Update patch_luci_app_store.sh

--- Scripts/patch_luci_app_store.sh
+++ Scripts/patch_luci_app_store.sh
@@ -16,9 +16,27 @@
 ##fi
 
 # 遍历 package 和 feeds 目录下所有 Makefile，修正 PKG_VERSION 和 PKG_RELEASE 格式
-find ./ -type f -name Makefile | while read MKFILE; do
+##find ./ -type f -name Makefile | while read MKFILE; do
+    ##if grep -qE '^PKG_VERSION:=[0-9]+\.[0-9]+\.[0-9]+-[0-9]+' "$MKFILE"; then
+        ##echo "Patching version in $MKFILE"
+        ##sed -i -E 's/^(PKG_VERSION:=)([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+)/\1\2\nPKG_RELEASE:=\3/' "$MKFILE"
+    ##fi
+##done
+
+
+SEARCH_ROOT=${1:-.}
+
+echo "patch_luci_app_store.sh: searching in $SEARCH_ROOT"
+
+FOUND=0
+find "$SEARCH_ROOT" -type f -name Makefile | while read MKFILE; do
     if grep -qE '^PKG_VERSION:=[0-9]+\.[0-9]+\.[0-9]+-[0-9]+' "$MKFILE"; then
         echo "Patching version in $MKFILE"
         sed -i -E 's/^(PKG_VERSION:=)([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+)/\1\2\nPKG_RELEASE:=\3/' "$MKFILE"
+        FOUND=1
     fi
 done
+
+if [ $FOUND -eq 0 ]; then
+    echo "No Makefile with PKG_VERSION:=x.y.z-n format found."
+fi
