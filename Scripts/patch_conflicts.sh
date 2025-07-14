773037dfd9c9e701818aba8dbcf4871901cb04a2
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2025-07-14 12:29:29 +0800

    Update patch_conflicts.sh

--- Scripts/patch_conflicts.sh
+++ Scripts/patch_conflicts.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-# Patch luci-app-openvpn-server & luci-app-openvpn: 禁止重复安装 /etc/config/openvpn
+# 1. 注释Makefile里的安装命令
 for PKGNAME in luci-app-openvpn-server luci-app-openvpn; do
   find . -type f -path "*/$PKGNAME/Makefile" | while read MK; do
     echo "Patching $MK"
     sed -i '/INSTALL_CONF.*openvpn.config.*etc\/config\/openvpn/s/^/#/' "$MK"
+    # 删除files/openvpn.config文件（如果存在）
+    PKGDIR=$(dirname "$MK")
+    if [ -f "$PKGDIR/files/openvpn.config" ]; then
+      echo "Removing $PKGDIR/files/openvpn.config"
+      rm -f "$PKGDIR/files/openvpn.config"
+    fi
   done
 done
 
-# Patch luci-app-socat: 禁止重复安装 /etc/config/socat
 find . -type f -path "*/luci-app-socat/Makefile" | while read MK; do
   echo "Patching $MK"
   sed -i '/INSTALL_CONF.*socat.config.*etc\/config\/socat/s/^/#/' "$MK"
+  PKGDIR=$(dirname "$MK")
+  if [ -f "$PKGDIR/files/socat.config" ]; then
+    echo "Removing $PKGDIR/files/socat.config"
+    rm -f "$PKGDIR/files/socat.config"
+  fi
 done
